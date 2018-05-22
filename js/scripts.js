15c1d0d65dda16b2d1e1c09d29e40102180aeaf5
Jacob <espeon132akajacob@gmail.com>
2018-05-21 21:04:46 -0700

    added some script

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
- Volume in drive C is Windows
- Volume Serial Number is EAC0-6EE5
+var myName = "Jacob Henry";
+var myDrive = "I want to be able to make my website look cool, fix a discord bot I'm making, and have a basis for python";
 
- Directory of C:\Users\Magma\Desktop\Jacob\Coding_Stuff\Classes[Taking]\JavaScript\JavaScript_Class\js
+console.log(
+  myName
+);
 
-05/21/2018  07:02 PM    <DIR>          .
-05/21/2018  07:02 PM    <DIR>          ..
-05/21/2018  07:02 PM                 0 scripts.js
-               1 File(s)              0 bytes
-               2 Dir(s)  1,911,185,133,568 bytes free
+console.log(
+  myDrive
+);
