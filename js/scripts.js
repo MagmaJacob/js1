2085652ee7b311b03678f0157499dd4bf6b1db10
Jacob <espeon132akajacob@gmail.com>
2018-05-21 20:17:46 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+ Volume in drive C is Windows
+ Volume Serial Number is EAC0-6EE5
+
+ Directory of C:\Users\Magma\Desktop\Jacob\Coding_Stuff\Classes[Taking]\JavaScript\JavaScript_Class\js
+
+05/21/2018  07:02 PM    <DIR>          .
+05/21/2018  07:02 PM    <DIR>          ..
+05/21/2018  07:02 PM                 0 scripts.js
+               1 File(s)              0 bytes
+               2 Dir(s)  1,911,185,133,568 bytes free
