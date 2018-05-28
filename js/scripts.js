d6491ba3da260afe7227884749d9c32d3f47e6d4
Jacob <espeon132akajacob@gmail.com>
2018-05-28 16:07:17 -0700

    Fixed wording

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,35 @@
-var myName = "Jacob Henry";
-var myDrive = "I want to be able to make my website look cool, fix a discord bot I'm making, and have a basis for python";
-var myHobbies = "I like to make music, code, and talk to friends";
-
+/*let favMovie = "Star Wars Trilogy"
+let favFood = "Pizza"
+const movieDate = new Date("05.25.1977")
+const foodIng = ['flour', 'water', 'cheese', 'toppings']
+alert(foodIng)
+confirm(favMovie + "|" + movieDate)
 console.log(
-  myName
+  favFood, favMovie, movieDate, foodIng
+);*/
+
+var firstName = "What is your first name";
+var result = window.prompt(firstName);
+console.log(
+  "[" + result + "] just logged in!!!"
 );
 
-console.log(
-  myDrive
-);
+var lastName = "What is your last name";
+var result1 = window.prompt(lastName);
+window.alert(result1);
 
-console.log(
-  myHobbies
-);
+var bday = new Date("05.25.1977");
+var result2 = window.confirm("Is your birthday " + bday);
+if(!result2) {
+  let result3 = window.prompt("Then when is your birthday");
+  alert(result3);
+  console.log(
+    "[" + result + "]'s birthday is " + result3
+  );
+};
+if(result2) {
+  alert("Good job! You were born when Star Wars was");
+  console.log(
+    "[" + result + "]'s birthday is the same as Star Wars'"
+  );
+};
