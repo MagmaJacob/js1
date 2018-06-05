9913962c47c4332c702d84f0bb675fc18aafde28
Jacob <espeon132akajacob@gmail.com>
2018-06-04 18:55:39 -0700

    made the master look pretty

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,1 @@
-var myName = "Jacob Henry";
-var myDrive = "I want to be able to make my website look cool, fix a discord bot I'm making, and have a basis for python";
-var myHobbies = "I like to make music, code, and talk to friends";
 
-console.log(
-  myName
-);
-
-console.log(
-  myDrive
-);
-
-console.log(
-  myHobbies
-);
